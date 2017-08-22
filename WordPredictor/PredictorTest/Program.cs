129f98e7c09350ff43ea66314130e71389fe447e
tyler.w.sriver <tyler.w.sriver@eagles.oc.edu>
2017-08-22 00:49:31 -0500

    Add Predictor C# Project

---
+++ WordPredictor/PredictorTest/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WordPredictor;
+
+namespace PredictorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiate predictor and train it
+            // ------------------------------------------------
+            Predictor target = new Predictor();
+            BookTrain(target, "/Users/tyler.w.sriver/Downloads/training.txt");
+            bool finish = false;
+
+            // -- Accept predict entries
+            // ------------------------------------------
+            while (!finish)
+            {
+                // Get Line and Print:
+                // ------------------------------------
+                string word = Console.ReadLine();
+                PrintPredictions(CleanEntries(target.Predict(word)));
+
+                // Ending?:
+                // -----------------------------------
+                if (word == "end")
+                {
+                    finish = true;
+                }
+            }
+
+        } // END
+
+        public static void BookTrain(Predictor target, string fileLocation)
+        {
+            string line;
+            int counter = 0;
+            StreamReader file = new StreamReader(fileLocation);
+            var beginTime = DateTime.Now;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                target.Predict(line);
+                Console.WriteLine("Predictor Trained on ... " + line);
+                counter++;
+            }
+            var endTime = DateTime.Now;
+            var duration = endTime - beginTime;
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Trained " + counter + " lines for " + duration.TotalSeconds + "s");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+        }
+
+        public static void PrintPredictions(List<string> predict)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Total Predictions: " + predict.Count);
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine();
+            for (int i = 0; i < predict.Count; i++)
+            {
+                if (i < 3)
+                {
+                    Console.Write(predict[i] + ", ");
+                }
+                else
+                {
+                    Console.Write(predict[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine();
+
+        }
+
+        public static List<string> CleanEntries(List<string> stringList)
+        {
+            List<string> clean = new List<string>();
+            foreach (string word in stringList)
+            {
+                string cleanWord = word.Replace(".", "");
+                cleanWord = word.Replace(",", "");
+                cleanWord = word.Replace("'", "");
+                cleanWord = word.Replace("\"", "");
+                cleanWord = word.Replace(")", "");
+                cleanWord = word.Replace("(", "");
+                cleanWord = word.Replace(":", "");
+                cleanWord = word.Replace(";", "");
+                cleanWord = word.Replace("?", "");
+                cleanWord = word.Replace("!", "");
+                cleanWord = word.Replace("\\", "");
+                cleanWord = word.Replace("/", "");
+                cleanWord = word.Replace("`", "");
+                cleanWord = word.Replace("-", "");
+                cleanWord = word.Replace("~", "");
+                cleanWord = word.Replace("*", "");
+                cleanWord = word.Replace("=", "");
+                cleanWord = word.Replace("+", "");
+                clean.Add(cleanWord);
+
+            }
+            return clean;
+        } 
+    }
+}
---
+++ WordPredictor/WordPredictor/Predictor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordPredictor
+{
+    public class Predictor
+    {
+        private readonly Dictionary<string, Dictionary<string, int>> 
+            _items = new Dictionary<string, Dictionary<string, int>>();
+        private readonly char[] _tokenDelimeter = {' '};
+        
+        /// <summary>
+        /// Train the predictor with a string, and return the 
+        /// predictions
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public List<string> Predict(string input)
+        {
+            var tokens = input.Split(_tokenDelimeter, StringSplitOptions.RemoveEmptyEntries);
+            var previousBuilder = new StringBuilder();
+            Dictionary<string, int> nextFullList;
+            foreach (var token in tokens)
+            {
+                nextFullList = GetOrCreate(_items, previousBuilder.ToString());
+                if (nextFullList.ContainsKey(token))
+                    nextFullList[token] += 1;
+                else
+                    nextFullList.Add(token, 1);
+
+                if (previousBuilder.Length > 0)
+                    previousBuilder.Append(" ");
+                previousBuilder.Append(token);
+            }
+            nextFullList = GetOrCreate(_items, previousBuilder.ToString());
+            var prediction = (from x in nextFullList
+                orderby x.Value descending
+                select x.Key).ToList();
+
+            return prediction;
+        }
+        
+        /// <summary>
+        /// Get or create the prediction
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="key"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private static T GetOrCreate<T>(Dictionary<string, T> d, string key)
+        {
+            if (d.ContainsKey(key))
+            {
+                return d[key];
+            }
+            var t = Activator.CreateInstance<T>();
+            d.Add(key, t);
+            return t;
+        }
+    }
+}
